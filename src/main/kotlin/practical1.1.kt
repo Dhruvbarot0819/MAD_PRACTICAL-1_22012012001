f029324db36241443521848e3143bc83d934bd4d
student <student@gnu.ac.in>
2023-07-17 10:40:28 +0530

    program is commited

--- src/main/kotlin/practical1.1.kt
+++ src/main/kotlin/practical1.1.kt
@@ -1,4 +1,17 @@
 fun main(){
-    println("hello world")
-    println("hello")
+//    println("hello world")
+//    println("hello")
+    var  number = 10
+    var name = "abc"
+    var c = 19.27
+    var db ='d'
+    var s = true
+
+    println ("$number is int var")
+    println ("$name is int var")
+    println ("$c is int var")
+    println ("$db is int var")
+    println ("$s is int var")
+
+
 }
