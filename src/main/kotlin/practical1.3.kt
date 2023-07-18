14eddf8311e243a32608b2bdfa18c4d7c203b1ec
student <student@gnu.ac.in>
2023-07-18 09:35:26 +0530

    program is commited

---
+++ src/main/kotlin/practical1.3.kt
@@ -0,0 +1,23 @@
+fun main(){
+    print("Enter your enrollment number")
+    var num = readLine()
+
+    print("Enter your name")
+    var name = readLine()
+
+    print("Enter your batch")
+    var batch = readLine()
+
+    print("Enter your branch")
+    var barnch = readLine()
+
+    print("Enter your age")
+    var age = readln()
+
+
+    println("your Enrollment number is$num")
+    println("your name is$name")
+    println("your batch is$batch")
+    println("your  barnch is$barnch")
+    println(" age$age")
+}
