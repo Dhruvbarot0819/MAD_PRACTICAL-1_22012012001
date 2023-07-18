3638ab2194eba84370b927392f8e9fdeea799fc4
student <student@gnu.ac.in>
2023-07-18 09:23:20 +0530

    program is commited

---
+++ src/main/kotlin/practical1.2.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var i: Int = 10
+    println("Integer to Double${i.toDouble()}")
+
+    var a: Double = 100.798
+    println("Double to Integer${a.toInt()}")
+
+    var b: String = "876"
+    println("string to Integer${b.toString()}")
+
+}
