50036c8da881c2b74fea8cb3d90a06e4dd2faca2
student <student@gnu.ac.in>
2023-07-18 09:58:55 +0530

    program is commited

--- src/main/kotlin/practical1.4.kt
+++ src/main/kotlin/practical1.4.kt
@@ -2,10 +2,11 @@ fun main(){
     print("Enter your number: ")
     var num:Int = readLine()!!.toInt()
 
-    if (num %2==0){
-        print("number is even")
-    }
-    else{
-        print("number is odd")
-    }
+//    if (num %2==0){
+//        print("number is even")
+//    }
+//    else{
+//        print("number is odd")
+//    }
+    println(if(num%2==0) "$num is Even" else "$num is Odd")
 }
